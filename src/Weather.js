c95b4ac2c3691493e05b36f53c8501959abf4298
Anastakas <kasilova@gmail.com>
2023-01-09 13:00:34 +0100

    added the Weather component and built the html structure

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,47 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
-      <h1>Hello from Weather</h1>
+      <div className="row">
+        <div className="col-5">
+          <h1>Kyiv</h1>
+        </div>
+        <div className="col-7">
+          <form>
+            <div className="row">
+              <div className="col-8">
+                <input type="search" placeholder="Type a city" />
+              </div>
+              <div className="col-4">
+                <input type="submit" value="Submit" />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <ul>
+        <li>Monday</li>
+        <li>12:05</li>
+      </ul>
+      <div className="row">
+        <div className="col-8">
+          <img
+            src="http://openweathermap.org/img/wn/04d@2x.png"
+            alt="Overcast clouds"
+          />
+          <strong>-3 ℃ </strong>
+          <spin>Overcast clouds</spin>
+        </div>
+        <div className="col-4">
+          <ul>
+            <li>Humidity: 92 %</li>
+            <li>Pressure: 1022 mb</li>
+            <li>Wind: 1 km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
