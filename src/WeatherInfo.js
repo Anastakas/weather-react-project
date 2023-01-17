d83d5320790883d6b1091a45820fd21d71b1f124
Anastakas <kasilova@gmail.com>
2023-01-17 15:29:26 +0100

    added extra component for the weather icon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
-
+import WeatherIcon from "./WeatherIcon";
 export default function WeatherInfo(props){
     return (
       <div className="WeatherInfo">
@@ -9,7 +9,7 @@ export default function WeatherInfo(props){
         <div className="row">
           <div className="col-8">
             <div className="d-flex">
-              <img src={props.data.iconUrl} alt={props.data.description} />
+              <WeatherIcon code={props.data.icon} />
 
               <span className="temperature">{props.data.temperature}</span>
               <span className="units">℃</span>
