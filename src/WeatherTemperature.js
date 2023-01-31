92d29e180c3e08da2da01ef8d17570940f0299e4
Anastakas <kasilova@gmail.com>
2023-01-31 13:31:06 +0100

    deleted convarsation from celsius to fahrenheit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,46 +1,15 @@
-import React, {useState} from "react";
+import React from "react";
 
 export default function WeatherTemperature (props){
-    const[unit, setUnit] = useState ("celsius");
-function showFahrenheit (event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-}
-function showCelsius (event){
-    event.preventDefault();
-    setUnit("celsius");
-}
-function fahrenheit(){
-return Math.round(props.celsius * 1.8 + 32);
-}
-
-    if (unit==="celsius"){
+ 
       return (
         <div className="WeatherTemperature d-flex">
           <span className="temperature">{props.celsius}</span>
           <span className="units">
-            ℃ |{" "}
-            <a href="/" onClick={showFahrenheit}>
-              ℉
-            </a>
+            ℃ 
+
           </span>
         </div>
       );
     }
-    else{
-        return (
-          <div className="WeatherTemperature d-flex">
-            <span className="temperature">
-              {fahrenheit()}
-            </span>
-            <span className="units">
-              <a href="/" onClick={showCelsius}>
-                {" "}
-                ℃
-              </a>{" "}
-              | ℉
-            </span>
-          </div>
-        );
-    }
-}
+  
